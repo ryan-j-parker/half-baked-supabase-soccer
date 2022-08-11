422ea305c30013159665cbc73c12f068c68903a0
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-11 11:14:48 -0700

    add functions and event listeners

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { renderGame } from './render-utils.js';
 
 const currentGameEl = document.getElementById('current-game-container');
 const pastGamesEl = document.getElementById('past-games-container');
-const logoutButton = document.getElementById('logout');
+//const logoutButton = document.getElementById('logout');
 
 const nameForm = document.getElementById('name-form');
 const teamOneAddButton = document.getElementById('team-one-add-button');
@@ -18,39 +18,54 @@ let name1 = '';
 let name2 = '';
 let score1 = 0;
 let score2 = 0;
-
+/*
+const newGame = {
+    name1: '',
+    name2: '',
+    score1: 0,
+    score2: 0,
+};
+*/
 nameForm.addEventListener('submit', (e) => {
     // don't forget to prevent the default form behavior!
-
+    e.preventDefault();
     // get the name data from the form
-
+    const data = new FormData(nameForm);
+    name1 = data.get('team-one');
+    name2 = data.get('team-two');
     // set the state to this data from the form
 
     // reset the form values
+
+    nameForm.reset();
 
     displayCurrentGameEl();
 });
 
 teamOneAddButton.addEventListener('click', () => {
     // increment the current state for team one's score
+    score1++;
 
     displayCurrentGameEl();
 });
 
 teamTwoAddButton.addEventListener('click', () => {
     // increment the current state for team two's score
+    score2++;
 
     displayCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
     // decrement the current state for team one's score
+    score1--;
 
     displayCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
     // decrement the current state for team two's score
+    score2--;
 
     displayCurrentGameEl();
 });
@@ -60,31 +75,59 @@ finishGameButton.addEventListener('click', async () => {
 
     // after creating this new game, re-fetch the games to get the updated state and display them (hint: call displayAllGames())
 
+    const currentGame = {
+        name1: name1,
+        name2: name2,
+        score1: score1,
+        score2: score2
+    };
+
+    await createGame(currentGame);
+
+    pastGamesEl.textContent = '';
+
     name1 = '';
     name2 = '';
     score1 = 0;
     score2 = 0;
 
     displayCurrentGameEl();
+
+    await displayAllGames();
+    displayCurrentGameEl();
 });
 
 // on load . . .
 window.addEventListener('', async () => {
     // display all past games (hint: call displayAllGames())
+    await displayAllGames();
+    displayCurrentGameEl();
 });
 
 function displayCurrentGameEl() {
     // clear out the current game div
+    currentGameEl.textContent = '';
     // change the label to show team one's name;
+    teamOneLabel.textContent = name1;
     // change the label to show team two's name;
+    teamTwoLabel.textContent = name2;
     // call the render game function to create a game element
+    const gameEl = renderGame({ name1, name2, score1, score2 });
     // append the element to the cleared out current game div
+    gameEl.classList.add('current-game');
+    currentGameEl.append(gameEl);
 }
 
-function displayAllGames() {
+async function displayAllGames() {
     // clear out the past games list in the DOM
+    currentGameEl.textContent = '';
     // FETCH ALL GAMES from supabase
+    const allGames = await getGames();
     // loop through the past games
+    for (let game of allGames) {
+        const pastGames = renderGame(game);
+        pastGamesEl.append(pastGames);
+    }
     // render and append a past game for each past game in state
 }
 
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_URL = 'https://ycrjdcltdpujspwklmtr.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljcmpkY2x0ZHB1anNwd2tsbXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2NDA4ODAsImV4cCI6MTk3NTIxNjg4MH0.09-eHnBOrLeSZ5iozNMkme5G9W9_LfVD2GYU4ycn4eg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,6 +10,7 @@ assumptions you can make:
 The table name in supabase is `games`
 
 The games are stored in the database using this data model:
+/
 {
    name1: ,
    name2: ,
@@ -20,13 +21,13 @@ The games are stored in the database using this data model:
 
 export async function createGame(game) {
     // create a single new game in the games table using the above object
-
+    const response = await client.from('games').insert(game);
     return checkError(response);
 }
 
 export async function getGames() {
     // select all games from the games table
-
+    const response = await client.from('games').select('*');
     return checkError(response);
 }
 
